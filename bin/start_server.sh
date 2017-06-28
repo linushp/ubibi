dbdbf8bae1cc3b17609335cbb560936de79878fa
luanhaipeng <910010653@qq.com>
2017-06-28 21:56:49 +0800

    aaa

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,6 +1,5 @@
 rm -f tpid
-set NODE_ENV=production
 
-nohup node ../server/ubibi_node_server.js > ~/logs/ubibi_node_server/ubibi_node_server.log 2>&1 &
+nohup NODE_ENV=production node ../server/ubibi_node_server.js > ~/logs/ubibi_node_server/ubibi_node_server.log 2>&1 &
 
 echo Start Success!
