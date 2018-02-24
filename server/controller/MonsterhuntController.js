193f546288063a1842a2bc919e1e98a71110c8fd
beecallorg@gmail.com <beecallorg@gmail.com>
2018-02-24 19:16:35 +0800

    aaa

--- server/controller/MonsterhuntController.js
+++ server/controller/MonsterhuntController.js
@@ -68,7 +68,29 @@ function sendHttpRequest(options,data){
     });
 }
 
+
+
 var login_token = "2f78e98cb8a86d0c292cb5111f112519";
+
+var login_code = "dafMEt37HsBBzDNh8yrDXEXxbUbVPfPHcbBaX7rJuNNZJ3m1vno2HNmQufikQGnTBFD9eRDsb1EZD8yC2C17Vmf";
+
+function get_getmonster_url(id) {
+    return "http://47.75.37.131:8396/monster-hunt/monster/getmonster?token="+login_token+"&needtoken=true&commandid=2&monsterid=" + id;
+}
+
+
+function doLogin() {
+    var LOGIN_URL = "http://47.75.37.131:8396/monster-hunt/user/login?token=default%20TOKEN&needtoken=true&commandid=11&code=" + login_code;
+    return  FileCacheReader.sendGetJsonRequest(LOGIN_URL).then(function (d) {
+        if(d && d.obj){
+            var token = d.obj.token;
+            login_token = token;
+
+        }
+        return d;
+    });
+}
+
 
 router.get("/getmonster",async function (req, res) {
 
@@ -79,8 +101,13 @@ router.get("/getmonster",async function (req, res) {
         return;
     }
 
-    var url = "http://47.75.37.131:8396/monster-hunt/monster/getmonster?token="+login_token+"&needtoken=true&commandid=2&monsterid=" + id;
+    var url = get_getmonster_url(id);
     var x = await FileCacheReader.sendGetJsonRequest(url);
+    if(x.code === -2){
+        await doLogin();
+        var url = get_getmonster_url(id);
+        x = await FileCacheReader.sendGetJsonRequest(url);
+    }
     res.send(x);
 });
 
@@ -88,6 +115,13 @@ router.get("/getmonster",async function (req, res) {
 router.get("/update_login_token",function (req, res) {
     var req_query = req.query;
     login_token = req_query.token || "";
+    res.send("ok")
+});
+
+
+router.get("/update_login_code",function (req, res) {
+    var req_query = req.query;
+    login_code = req_query.code || "";
     res.send("ok")
 });
 
