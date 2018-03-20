71b4e6d8be3a7d1b6a4de9dc364355a1ec4d9366
luan <master@ubibi.cn>
2018-03-20 16:28:56 +0800

    dev

---
+++ server/services/SysConfigService.js
@@ -0,0 +1,41 @@
+var SqlQueryUtils = require('../utils/SqlQueryUtils');
+
+
+var SysConfigItemModel = {
+    tableName: 's_config',
+    tableFields: [
+        'id',
+        'config_name',
+        'config_value',
+        'create_time',
+        'update_time'
+    ]
+};
+
+
+
+async function insertOrUpdate(config_name,config_value) {
+
+    var itemObject = {
+        config_name:config_name,
+        config_value:config_value
+    };
+
+    var mm = await SqlQueryUtils.doQueryByConditionAsync(SysConfigItemModel,{config_name:config_name});
+    if(mm && mm.result && mm.result.length > 0){
+        var id = mm.result[0].id;
+        return SqlQueryUtils.doUpdateByModelAsync(SysConfigItemModel,itemObject,id);
+    }else {
+        return SqlQueryUtils.doInsertByModelAsync(SysConfigItemModel,itemObject);
+    }
+}
+
+async function getConfigByName(config_name) {
+    return SqlQueryUtils.doQueryByConditionAsync(SysConfigItemModel,{config_name:config_name});
+}
+
+
+module.exports = {
+    insertOrUpdate:insertOrUpdate,
+    getConfigByName:getConfigByName
+};
